24ecc4dab43e0dc9e97e3aeac11d3396dc51e037
Izzette <isabellcowan@gmail.com>
2015-05-30 21:20:47 -0500

    v0.0.3 - Build passing   * lnls/replace up

--- lib/lsys.c
+++ lib/lsys.c
@@ -1,21 +1,24 @@
+#include <stdlib.h>
+
 #include "lsys.h"
+#include "lnls.h"
 
 struct node *string;
-char *ruleset[];
+char **ruleset;
 
-void init_string(char *axiom){
+void init_string (char *axiom) {
 	string = malloc (sizeof (struct node));
-	string.c = '\0';
-	string.next = NULL;
+	string->c = '\0';
+	string->next = NULL;
 
 	struct node *current = string;
 
 	while ('\0' != *axiom) {
-		struct node next = malloc (sizeof (struct node));
-		next.c = *axiom;
-		next.next = NULL;
-		current.next = &next;
-		current = current.next;
+		struct node *next = malloc (sizeof (struct node));
+		next->c = *axiom;
+		next->next = NULL;
+		current->next = next;
+		current = current->next;
 		++axiom;
 	}
 }
